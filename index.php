71596f48ccc0bbf6e768dbb4eb58db01fc18b3b0
xzen769 <xzen769@gmail.com>
2024-04-10 10:13:02 +0200

    refactor(Messages): delete Messages class controller

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ require 'vendor/autoload.php';
 
 use App\Router;
 use App\Controllers\User;
-use App\Controllers\Messages;
 
 new Router([
-  'user/:id' => User::class,
-  'messages' => Messages::class
+  'user/:id' => User::class
 ]);
